821c93b2e20b71317ef6cfd513e6250458295a62
ndepallo5399 <92735300+ndepallo5399@users.noreply.github.com>
2023-02-08 10:16:54 -0500

    CIS276 Obstacle Course

---
+++ CIS276_Nick_DePallo_ObstacleCourse/Assets/ObstacleCoursePack/Controller/CharacterControls.cs
@@ -0,0 +1,196 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+[RequireComponent (typeof (CapsuleCollider))]
+
+public class CharacterControls : MonoBehaviour {
+	
+	public float speed = 10.0f;
+	public float airVelocity = 8f;
+	public float gravity = 10.0f;
+	public float maxVelocityChange = 10.0f;
+	public float jumpHeight = 2.0f;
+	public float maxFallSpeed = 20.0f;
+	public float rotateSpeed = 25f;
+	private Vector3 moveDir;
+	public GameObject cam;
+	private Rigidbody rb;
+
+	private float distToGround;
+
+	private bool canMove = true;
+	private bool isStuned = false;
+	private bool wasStuned = false;
+	private float pushForce;
+	private Vector3 pushDir;
+
+	public Vector3 checkPoint;
+	private bool slide = false;
+
+	void  Start (){
+		distToGround = GetComponent<Collider>().bounds.extents.y;
+	}
+	
+	bool IsGrounded (){
+		return Physics.Raycast(transform.position, -Vector3.up, distToGround + 0.1f);
+	}
+	
+	void Awake () {
+		rb = GetComponent<Rigidbody>();
+		rb.freezeRotation = true;
+		rb.useGravity = false;
+
+		checkPoint = transform.position;
+		Cursor.visible = false;
+	}
+	
+	void FixedUpdate () {
+		if (canMove)
+		{
+			if (moveDir.x != 0 || moveDir.z != 0)
+			{
+				Vector3 targetDir = moveDir;
+
+				targetDir.y = 0;
+				if (targetDir == Vector3.zero)
+					targetDir = transform.forward;
+				Quaternion tr = Quaternion.LookRotation(targetDir);
+				Quaternion targetRotation = Quaternion.Slerp(transform.rotation, tr, Time.deltaTime * rotateSpeed);
+				transform.rotation = targetRotation;
+			}
+
+			if (IsGrounded())
+			{
+				Vector3 targetVelocity = moveDir;
+				targetVelocity *= speed;
+
+				Vector3 velocity = rb.velocity;
+				if (targetVelocity.magnitude < velocity.magnitude)
+				{
+					targetVelocity = velocity;
+					rb.velocity /= 1.1f;
+				}
+				Vector3 velocityChange = (targetVelocity - velocity);
+				velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+				velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+				velocityChange.y = 0;
+				if (!slide)
+				{
+					if (Mathf.Abs(rb.velocity.magnitude) < speed * 1.0f)
+						rb.AddForce(velocityChange, ForceMode.VelocityChange);
+				}
+				else if (Mathf.Abs(rb.velocity.magnitude) < speed * 1.0f)
+				{
+					rb.AddForce(moveDir * 0.15f, ForceMode.VelocityChange);
+				}
+
+				if (IsGrounded() && Input.GetButton("Jump"))
+				{
+					rb.velocity = new Vector3(velocity.x, CalculateJumpVerticalSpeed(), velocity.z);
+				}
+			}
+			else
+			{
+				if (!slide)
+				{
+					Vector3 targetVelocity = new Vector3(moveDir.x * airVelocity, rb.velocity.y, moveDir.z * airVelocity);
+					Vector3 velocity = rb.velocity;
+					Vector3 velocityChange = (targetVelocity - velocity);
+					velocityChange.x = Mathf.Clamp(velocityChange.x, -maxVelocityChange, maxVelocityChange);
+					velocityChange.z = Mathf.Clamp(velocityChange.z, -maxVelocityChange, maxVelocityChange);
+					rb.AddForce(velocityChange, ForceMode.VelocityChange);
+					if (velocity.y < -maxFallSpeed)
+						rb.velocity = new Vector3(velocity.x, -maxFallSpeed, velocity.z);
+				}
+				else if (Mathf.Abs(rb.velocity.magnitude) < speed * 1.0f)
+				{
+					rb.AddForce(moveDir * 0.15f, ForceMode.VelocityChange);
+				}
+			}
+		}
+		else
+		{
+			rb.velocity = pushDir * pushForce;
+		}
+		rb.AddForce(new Vector3(0, -gravity * GetComponent<Rigidbody>().mass, 0));
+	}
+
+	private void Update()
+	{
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		Vector3 v2 = v * cam.transform.forward;
+		Vector3 h2 = h * cam.transform.right;
+		moveDir = (v2 + h2).normalized;
+
+		RaycastHit hit;
+		if (Physics.Raycast(transform.position, -Vector3.up, out hit, distToGround + 0.1f))
+		{
+			if (hit.transform.tag == "Slide")
+			{
+				slide = true;
+			}
+			else
+			{
+				slide = false;
+			}
+		}
+	}
+
+	float CalculateJumpVerticalSpeed () {
+		return Mathf.Sqrt(2 * jumpHeight * gravity);
+	}
+
+	public void HitPlayer(Vector3 velocityF, float time)
+	{
+		rb.velocity = velocityF;
+
+		pushForce = velocityF.magnitude;
+		pushDir = Vector3.Normalize(velocityF);
+		StartCoroutine(Decrease(velocityF.magnitude, time));
+	}
+
+	public void LoadCheckPoint()
+	{
+		transform.position = checkPoint;
+	}
+
+	private IEnumerator Decrease(float value, float duration)
+	{
+		if (isStuned)
+			wasStuned = true;
+		isStuned = true;
+		canMove = false;
+
+		float delta = 0;
+		delta = value / duration;
+
+		for (float t = 0; t < duration; t += Time.deltaTime)
+		{
+			yield return null;
+			if (!slide)
+			{
+				pushForce = pushForce - Time.deltaTime * delta;
+				pushForce = pushForce < 0 ? 0 : pushForce;
+			}
+			rb.AddForce(new Vector3(0, -gravity * GetComponent<Rigidbody>().mass, 0));
+		}
+
+		if (wasStuned)
+		{
+			wasStuned = false;
+		}
+		else
+		{
+			isStuned = false;
+			canMove = true;
+		}
+	}
+
+	private void OnTriggerEnter(Collider other)
+	{
+		Debug.Log("YOU HAVE FINISHED AND WON!!!");
+	}
+}
